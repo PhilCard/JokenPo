a79440064339e8d25c504ebfa976f4ddad64c08d
unknown <philcard.26@gmail.com>
2025-07-23 01:39:18 -0300

    cardspo apagado

--- src/cardspo.js
+++
@@ -1,5 +0,0 @@
-export const cards = [
-  {id: 1, img: "assets/img/icons/fist.png", titulo:"Pedra"},
-  {id: 2, img: "assets/img/icons/hand-paper.png", titulo:"Papel"},
-  {id: 3, img: "assets/img/icons/scissors.png", titulo:"Tesoura"}
-];
